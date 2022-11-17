aa9cb1a2508b2fd51c32ba88ea26ac8ce5979dda
Matthewf11 <matthew.fleischer@sky.uk>
2022-11-17 14:29:50 +0000

    Initial Commit

---
+++ API-ProgrammaticUI-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API-ProgrammaticUI-Practice
+//
+//  Created by Matthew David Fleischer on 17/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ API-ProgrammaticUI-PracticeUITests/API_ProgrammaticUI_PracticeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  API_ProgrammaticUI_PracticeUITestsLaunchTests.swift
+//  API-ProgrammaticUI-PracticeUITests
+//
+//  Created by Matthew David Fleischer on 17/11/2022.
+//
+
+import XCTest
+
+final class API_ProgrammaticUI_PracticeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
