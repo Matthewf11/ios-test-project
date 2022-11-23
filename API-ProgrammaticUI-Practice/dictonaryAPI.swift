8317e6ccb242d1d7073baad9df67d8083f0d6fb3
Matthewf11 <matthew.fleischer@sky.uk>
2022-11-23 10:55:15 +0000

    change

--- API-ProgrammaticUI-Practice/dictonaryAPI.swift
+++ API-ProgrammaticUI-Practice/dictonaryAPI.swift
@@ -25,39 +25,58 @@ struct Definition:Codable {
     var thumbs_down:Int?
 }
 
+protocol DictonaryResponse {
+    func queryDictonary(completion: @escaping (Definitions?) -> Void, query:String)
+}
 
 
-func queryDictonary(completion: @escaping (Definitions?) -> Void, query:String) {
-    let headers = [
-        "X-RapidAPI-Key": "0a7e318632msh40a8c872dd77589p1efba8jsn65809613d804",
-        "X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com"
-    ]
-
-    let request = NSMutableURLRequest(url: NSURL(string: "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=\(query)")! as URL,
-                                            cachePolicy: .useProtocolCachePolicy,
-                                        timeoutInterval: 10.0)
-    request.httpMethod = "GET"
-    request.allHTTPHeaderFields = headers
-
-    let session = URLSession.shared
-    let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-        if (error != nil) {
-            print(error)
-        } else {
-            var result:Definitions?
-            if let data = data {
-                do{
-                    result = try JSONDecoder().decode(Definitions.self, from: data)
+class DictonaryApi:DictonaryResponse{
+    func queryDictonary(completion: @escaping (Definitions?) -> Void, query:String) {
+        let headers = [
+            "X-RapidAPI-Key": "0a7e318632msh40a8c872dd77589p1efba8jsn65809613d804",
+            "X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com"
+        ]
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=\(query)")! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            } else {
+                var result:Definitions?
+                if let data = data {
+                    do{
+                        result = try JSONDecoder().decode(Definitions.self, from: data)
+                    }
+                    catch{
+                        print("Failed to convert: \(error.localizedDescription)")
+                    }
+                    
+                    completion(result)
                 }
-                catch{
-                    print("Failed to convert: \(error.localizedDescription)")
-                }
-              
-                completion(result)
+                
             }
-            
-        }
-    })
-
-    dataTask.resume()
+        })
+        
+        dataTask.resume()
+    }
+}
+class MockDictionaryAPI:DictonaryResponse {
+    func queryDictonary(completion: @escaping (Definitions?) -> Void, query:String){
+        var result:Definitions?
+        
+        result?.list = [Definition(thumbs_up: 10, sound_urls: [""]),
+                  Definition(thumbs_up: 20, sound_urls: [""]),
+                  Definition(thumbs_up: 30, sound_urls: [""]),
+                  Definition(thumbs_up: 40, sound_urls: [""]),
+                  Definition(thumbs_up: 50, sound_urls: [""]),
+                  Definition(thumbs_up: 60, sound_urls: [""]),
+                  Definition(thumbs_up: 70, sound_urls: [""])]
+        completion(result)
+    }
 }
